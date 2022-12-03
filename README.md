3e2831c0ce8f012032da86bea1600335d702ae30
Jesse <58728143+Jessuhh@users.noreply.github.com>
2022-12-03 22:28:53 +0100

    Release 1.0.0

--- README.md
+++ README.md
@@ -2,26 +2,30 @@
 
 <p align="center">Generate discord timestamps using the Flow Launcher</p>
 
-<p align="center"><img src="https://ik.imagekit.io/698xlahbaqz/2022-08-01-20-33-19_Trim_J5xoh2zfd.gif?ik-sdk-version=javascript-1.4.3&updatedAt=1659397450274" /><p>
+<p align="center"><img src="https://user-images.githubusercontent.com/58728143/205462441-2ff1a5c7-26d8-485f-9e7d-f5cf1b4d5f7a.gif" /><p>
 
 <p align="center">Built with <a href="https://github.com/gabrielcarloto/flow-launcher-helper">flow-launcher-helper</a></p>
 <p align="center">Huge thanks to <a href="https://github.com/gabrielcarloto/flow-search-npm">gabrielcarloto</a> for a TypeScript Flow Launcher plugin example</p>
 
 ## Usage
-
+Type `dt` and enter a time reference to get started (see the gif above for several examples). Click or press enter to copy the timestamp to your clipboard; ready to be pasted into discord!  
 
 ## Installation
 
-This plugin is written in Typescript, so you need Node.js installed to use it.
+This plugin is written in Typescript, so you need to have Node.js installed to use it.
 
 ### Install Node.js
 
 To install Node.js, go to https://nodejs.org/ and download the LTS (Long Term Support) version. Alternatively, you can use [nvm](https://github.com/nvm-sh/nvm) (or [nvm-windows](https://github.com/coreybutler/nvm-windows)).
 
-<!-- ### Install the plugin
+
+
+<!--
+### Install the plugin
 
 You can find this plugin in Flow's Plugin Store, or by running this command in Flow Launcher:
 
 ```
 pm install discord timestamps
-``` -->
+``` 
+-->
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Flow, JSONRPCResponse } from "flow-launcher-helper";
 import ms from "ms";
 import dayjs from "dayjs";
-import "dayjs/locale/nl.js";
 import relativeTime from "dayjs/plugin/relativeTime.js";
 import localizedFormat from "dayjs/plugin/localizedFormat.js";
 import childProcess from "child_process";
+import * as chrono from "chrono-node";
 
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
@@ -16,81 +16,84 @@ type Methods = "copy_result";
 const { params, on, showResult, run } = new Flow("app.png");
 
 on("query", () => {
-    let message = "";
-
     let date: dayjs.Dayjs = dayjs();
 
-    try {
-        const timestamp = dayjs(params);
-
-        if (!timestamp.isValid()) {
-            message = "using ms";
-            date = dayjs(Date.now() + ms(params), ["HH:mm"]);
-        } else {
-            message = "using dayjs";
-            date = timestamp;
-        }
-
-        const results: JSONRPCResponse<Methods>[] = [
-            {
-                title: date.format("L"),
-                subtitle: `<t:${date.unix()}:d>`,
-                method: "copy_result",
-                params: [`<t:${date.unix()}:d>`],
-            },
-            {
-                title: date.format("LL"),
-                subtitle: `<t:${date.unix()}:D>`,
-                method: "copy_result",
-                params: [`<t:${date.unix()}:D>`],
-            },
-            {
-                title: date.format("LT"),
-                subtitle: `<t:${date.unix()}:t>`,
-                method: "copy_result",
-                params: [`<t:${date.unix()}:t>`],
-            },
-            {
-                title: date.format("LTS"),
-                subtitle: `<t:${date.unix()}:T>`,
-                method: "copy_result",
-                params: [`<t:${date.unix()}:T>`],
-            },
-            {
-                title: date.format("LLL"),
-                subtitle: `<t:${date.unix()}:f>`,
-                method: "copy_result",
-                params: [`<t:${date.unix()}:f>`],
-            },
-            {
-                title: date.format("LLLL"),
-                subtitle: `<t:${date.unix()}:F>`,
-                method: "copy_result",
-                params: [`<t:${date.unix()}:F>`],
-            },
-            {
-                title: date.fromNow(),
-                subtitle: `<t:${date.unix()}:R>`,
-                method: "copy_result",
-                params: [`<t:${date.unix()}:R>`],
-            },
-            {
-                title: date.unix().toString(),
-                subtitle: date.unix().toString(),
-                method: "copy_result",
-                params: [date.unix().toString()],
-            },
-        ];
-
-        showResult(...results);
-    } catch (error) {
-        message = "User input was not valid";
-        // Tell the user input is not recognized
-        showResult({
-            title: message,
-        });
+    // Use chrono
+    if (params.length > 0) {
+        const timestamp = chrono.parseDate(params);
+        date = dayjs(timestamp);
     }
+
+    if (date.isValid()) {
+        showResult(...getResults(date));
+        return;
+    }
+
+    // Parse through ms
+    date = dayjs(new Date(Date.now() + ms(params)));
+
+    if (date.isValid()) {
+        showResult(...getResults(date));
+        return;
+    }
+
+    showResult({
+        title: "Invalid input",
+    });
 });
+
+function getResults(date: dayjs.Dayjs): JSONRPCResponse<Methods>[] {
+    return [
+        {
+            title: date.format("L"),
+            subtitle: `<t:${date.unix()}:d>`,
+            method: "copy_result",
+            params: [`<t:${date.unix()}:d>`],
+        },
+        {
+            title: date.format("LL"),
+            subtitle: `<t:${date.unix()}:D>`,
+            method: "copy_result",
+            params: [`<t:${date.unix()}:D>`],
+        },
+        {
+            title: date.format("LT"),
+            subtitle: `<t:${date.unix()}:t>`,
+            method: "copy_result",
+            params: [`<t:${date.unix()}:t>`],
+        },
+        {
+            title: date.format("LTS"),
+            subtitle: `<t:${date.unix()}:T>`,
+            method: "copy_result",
+            params: [`<t:${date.unix()}:T>`],
+        },
+        {
+            title: date.format("LLL"),
+            subtitle: `<t:${date.unix()}:f>`,
+            method: "copy_result",
+            params: [`<t:${date.unix()}:f>`],
+        },
+        {
+            title: date.format("LLLL"),
+            subtitle: `<t:${date.unix()}:F>`,
+            method: "copy_result",
+            params: [`<t:${date.unix()}:F>`],
+        },
+        {
+            title: date.fromNow(),
+            subtitle: `<t:${date.unix()}:R>`,
+            method: "copy_result",
+            params: [`<t:${date.unix()}:R>`],
+        },
+        {
+            title: date.unix().toString(),
+            subtitle: date.unix().toString(),
+            method: "copy_result",
+            params: [date.unix().toString()],
+        },
+    ];
+}
 
 on("copy_result", () => {
     copy(params);
