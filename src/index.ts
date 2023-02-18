f16382e9d6157fb32db5fd5e12b8d08cd72b61cd
Joël Kuijper <jmkuijper@outlook.com>
2023-02-18 12:38:46 +0100

    feat: use new nodejs compatibility

--- src/index.ts
+++ src/index.ts
@@ -16,87 +16,87 @@ type Methods = "copy_result";
 const { params, on, showResult, run } = new Flow("app.png");
 
 on("query", () => {
-    let date: dayjs.Dayjs = dayjs();
+  let date: dayjs.Dayjs = dayjs();
 
-    // Use chrono
-    if (params.length > 0) {
-        const timestamp = chrono.parseDate(params);
-        date = dayjs(timestamp);
-    }
+  // Use chrono
+  if (params.length > 0) {
+    const timestamp = chrono.parseDate(params);
+    date = dayjs(timestamp);
+  }
 
-    if (date.isValid()) {
-        showResult(...getResults(date));
-        return;
-    }
+  if (date.isValid()) {
+    showResult(...getResults(date));
+    return;
+  }
 
-    // Parse through ms
-    date = dayjs(new Date(Date.now() + ms(params)));
+  // Parse through ms
+  date = dayjs(new Date(Date.now() + ms(params)));
 
-    if (date.isValid()) {
-        showResult(...getResults(date));
-        return;
-    }
+  if (date.isValid()) {
+    showResult(...getResults(date));
+    return;
+  }
 
-    showResult({
-        title: "Invalid input",
-    });
+  showResult({
+    title: "Invalid input",
+  });
 });
 
 function getResults(date: dayjs.Dayjs): JSONRPCResponse<Methods>[] {
-    return [
-        {
-            title: date.format("L"),
-            subtitle: `<t:${date.unix()}:d>`,
-            method: "copy_result",
-            params: [`<t:${date.unix()}:d>`],
-        },
-        {
-            title: date.format("LL"),
-            subtitle: `<t:${date.unix()}:D>`,
-            method: "copy_result",
-            params: [`<t:${date.unix()}:D>`],
-        },
-        {
-            title: date.format("LT"),
-            subtitle: `<t:${date.unix()}:t>`,
-            method: "copy_result",
-            params: [`<t:${date.unix()}:t>`],
-        },
-        {
-            title: date.format("LTS"),
-            subtitle: `<t:${date.unix()}:T>`,
-            method: "copy_result",
-            params: [`<t:${date.unix()}:T>`],
-        },
-        {
-            title: date.format("LLL"),
-            subtitle: `<t:${date.unix()}:f>`,
-            method: "copy_result",
-            params: [`<t:${date.unix()}:f>`],
-        },
-        {
-            title: date.format("LLLL"),
-            subtitle: `<t:${date.unix()}:F>`,
-            method: "copy_result",
-            params: [`<t:${date.unix()}:F>`],
-        },
-        {
-            title: date.fromNow(),
-            subtitle: `<t:${date.unix()}:R>`,
-            method: "copy_result",
-            params: [`<t:${date.unix()}:R>`],
-        },
-        {
-            title: date.unix().toString(),
-            subtitle: date.unix().toString(),
-            method: "copy_result",
-            params: [date.unix().toString()],
-        },
-    ];
+  return [
+    {
+      title: date.format("L"),
+      subtitle: `<t:${date.unix()}:d>`,
+      method: "copy_result",
+      params: [`<t:${date.unix()}:d>`],
+    },
+    {
+      title: date.format("LL"),
+      subtitle: `<t:${date.unix()}:D>`,
+      method: "copy_result",
+      params: [`<t:${date.unix()}:D>`],
+    },
+    {
+      title: date.format("LT"),
+      subtitle: `<t:${date.unix()}:t>`,
+      method: "copy_result",
+      params: [`<t:${date.unix()}:t>`],
+    },
+    {
+      title: date.format("LTS"),
+      subtitle: `<t:${date.unix()}:T>`,
+      method: "copy_result",
+      params: [`<t:${date.unix()}:T>`],
+    },
+    {
+      title: date.format("LLL"),
+      subtitle: `<t:${date.unix()}:f>`,
+      method: "copy_result",
+      params: [`<t:${date.unix()}:f>`],
+    },
+    {
+      title: date.format("LLLL"),
+      subtitle: `<t:${date.unix()}:F>`,
+      method: "copy_result",
+      params: [`<t:${date.unix()}:F>`],
+    },
+    {
+      title: date.fromNow(),
+      subtitle: `<t:${date.unix()}:R>`,
+      method: "copy_result",
+      params: [`<t:${date.unix()}:R>`],
+    },
+    {
+      title: date.unix().toString(),
+      subtitle: date.unix().toString(),
+      method: "copy_result",
+      params: [date.unix().toString()],
+    },
+  ];
 }
 
 on("copy_result", () => {
-    copy(params);
+  copy(params);
 });
 
 run();
